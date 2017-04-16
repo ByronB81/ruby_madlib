12c7314e36dd2a1b615b01a006c0d8a8a64906ec
Byron Blocker <byronblocker@gmail.com>
2017-04-15 17:17:00 -0700

    fix error making variables appear in brackets

--- app.rb
+++ app.rb
@@ -10,11 +10,12 @@ get('/quiz') do
 end
 
 get('/story') do
-  *@char1 = params.fetch('char1')
-  *@char2 = params.fetch('char2')
-  *@animal = params.fetch('animal')
-  *@exclamation = params.fetch('exclamation')
-  *@verb = params.fetch('verb')
-  *@place = params.fetch('place')
+  @char1 = params.fetch('char1')
+  @char2 = params.fetch('char2')
+  @animal = params.fetch('animal')
+  @exclamation = params.fetch('exclamation')
+  @verb = params.fetch('verb')
+  @place = params.fetch('place')
   erb(:story)
 end
+
