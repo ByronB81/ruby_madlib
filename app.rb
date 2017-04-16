ffa55f797f486ee23e80b1e237cf20aa181ce52e
Byron Blocker <byronblocker@gmail.com>
2017-04-15 17:09:53 -0700

    finish quiz and story page

--- app.rb
+++ app.rb
@@ -5,6 +5,16 @@ get('/') do
   erb(:home)
 end
 
-get('quiz') do
+get('/quiz') do
   erb(:quiz)
+end
+
+get('/story') do
+  *@char1 = params.fetch('char1')
+  *@char2 = params.fetch('char2')
+  *@animal = params.fetch('animal')
+  *@exclamation = params.fetch('exclamation')
+  *@verb = params.fetch('verb')
+  *@place = params.fetch('place')
+  erb(:story)
 end
